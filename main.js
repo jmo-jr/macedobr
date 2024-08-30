494882a40ddb125d06d9dd064f1aeac5f8c79847
Jorge Monteiro Jr <junior@w3is.net>
2024-08-29 22:12:09 -0300

    separei transparencia

--- main.js
+++ main.js
@@ -12,10 +12,16 @@ const { createApp } = Vue
 					],
 					certificados: [],
 					chamamentos: [],
+					estatuto: [
+						{
+							name: 'Estatuto Associação Macedo Brasil',
+							link: "https://drive.google.com/file/d/19UFbrH_LkfIr860cSNd8T7usKqysHZ7j/preview"
+						}
+					],
 					regulamentos: [
 						{
 							name: "Proposta de Regulamento de Contratação de Pessoal",
-							link: "https://drive.google.com/uc?export=download&id=1xcPvXdCUKkpmYuu2Ltyo4eh2_jAwI2GW"
+							link: "https://drive.google.com/file/1xcPvXdCUKkpmYuu2Ltyo4eh2_jAwI2GW/preview"
 						},
 						{
 							name: "Regulamento de Compra AMBR",
